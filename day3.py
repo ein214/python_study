9040f99fdd339a75fe87159508ab2c544d19b5cf
Eunji Ko <einee214@gmail.com>
2022-03-25 21:34:47 +0900

    day3 자료형 마스터

---
+++ day3.py
@@ -0,0 +1,42 @@
+# 3-1 변수
+name = "Bob"
+print(name)
+name = "Kelly"
+print(name)
+name = "Jin"
+print(name)
+
+a = 10
+b = a
+print(b)
+
+# 3-3 숫자형
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(5.9))
+print(type(0.111))
+
+print(2000+23)
+print("2000" + "23")
+print(0.5 + 0.5)
+print("0.5" + "0.5")
+
+print(type(2000 + 23))
+print(type("2000" + "23"))
+
+# 3-4 불 자료형
+print(2 > 3)
+print(2 < 9)
+
+# 3-5 변수 이름 규칙 
+"""
+꼭 지켜야되는 규칙
+변수명은 숫자로 시작할 수 없다.
+변수명에는 공백 사용 불가
+예약어는 변수명으로 사용불가
+
+지키지 않아도 되지만 권장하는 규칙
+변수명은 스네이크 규칙
+"""
+
---
+++ day3_homework.py
@@ -0,0 +1,9 @@
+title = "당근앱 만들기"
+students = 214
+rank = 3.1
+is_free = False
+
+print(type(title))
+print(type(students))
+print(type(rank))
+print(type(is_free))
